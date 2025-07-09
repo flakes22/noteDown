d5c99f451a4cda42aa9c415a374c1ed8c51d4bff
Mahek <mahek22d@gmail.com>
2025-07-09 11:44:49 +0530

    logo fixes

--- notes-app/src/components/Navbar/index.jsx
+++ notes-app/src/components/Navbar/index.jsx
@@ -1,10 +1,10 @@
 import logo from "../../assets/logo.png";
 
 export const Navbar = () => (
-  <header>
-    <div>
-      <img src={logo} alt="logo" />
+  <header className="flex p-5">
+    <div className="w-14 h-14">
+      <img className="w-full h-full" src={logo} alt="logo" />
     </div>
-    <h1>NoteDown</h1>
+    <h1 className=" text-4xl">NoteDown</h1>
   </header>
 );
