14bfacf4e890e439218c4b45f56b0d5d0c24a321
Mahek <mahek22d@gmail.com>
2025-07-18 22:14:02 +0530

    navbar made

--- notes-app/src/components/Navbar/index.jsx
+++ notes-app/src/components/Navbar/index.jsx
@@ -1,10 +1,10 @@
 import logo from "../../assets/logo.png";
 
 export const Navbar = () => (
-  <header className="flex p-5">
-    <div className="w-14 h-14">
+  <header className="flex px-3 py-2 gap-3 border-b-2  border-amber-700">
+    <div className="w-12 h-12">
       <img className="w-full h-full" src={logo} alt="logo" />
     </div>
-    <h1 className=" text-4xl">NoteDown</h1>
+    <h1 className=" text-amber-800 text-4xl italic font-semibold">NoteDown</h1>
   </header>
 );
