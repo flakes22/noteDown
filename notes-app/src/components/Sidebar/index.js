b60b9546f739c808b2d2bb387d169bf5fca014a9
Mahek <mahek22d@gmail.com>
2025-07-19 22:28:59 +0530

    sidebar aligned

--- notes-app/src/components/Sidebar/index.js
+++ notes-app/src/components/Sidebar/index.js
@@ -1,11 +1,25 @@
-import {Link} from 'react-router-dom';
-export const Sidebar=()=>{
-    return(
-        <aside className='flex flex-col gap-3 border-r-2 border-amber-800 w-24 h-screen p-3'>
-            <Link to="/">Home</Link>
-            <Link to="/archive">Archive</Link>
-            <Link to="important">Important</Link>
-            <Link to="bin">Bin</Link>
+import { Link } from 'react-router-dom';
+export const Sidebar = () => {
+    return (
+        <aside className='flex flex-col gap-6 border-r-2 border-amber-800 h-screen p-4'>
+            <Link className="flex align-center gap-1"to="/" >
+                <span class="material-symbols-outlined">home</span>
+                <span>Home</span></Link>
+            <Link className="flex align-center gap-1" to="/archive">
+                <span class="material-symbols-outlined">
+                    archive
+                </span>
+                <span>Archive</span>
+            </Link>
+            <Link className="flex align-center gap-1" to="important">
+            <span class="material-symbols-outlined">
+label_important
+</span><span>Important</span></Link>
+            <Link className="flex align-center gap-1" to="bin">
+            <span class="material-symbols-outlined">
+                    delete
+                </span>
+                <span>Bin</span></Link>
         </aside>
 
     )
